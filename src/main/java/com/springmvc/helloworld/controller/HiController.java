a075d8693a7b9b9292290497d3cf743fa7979bc0
hwzhangm <hwzhangm@isoftstone.com>
2017-12-27 20:31:56 +0800

    update

--- src/main/java/com/springmvc/helloworld/controller/HiController.java
+++ src/main/java/com/springmvc/helloworld/controller/HiController.java
@@ -1,6 +1,7 @@
 package com.springmvc.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +15,12 @@ public class HiController {
     @RequestMapping(value = "/helloworld", method = RequestMethod.GET, params = {"username","age!=10"}, headers = {"Accept-Lanaguage"})
     public String hi(){
         System.out.println("hello world");
+        return "success";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id){
+        System.out.println("this id is " + id);
         return "success";
     }
 }
