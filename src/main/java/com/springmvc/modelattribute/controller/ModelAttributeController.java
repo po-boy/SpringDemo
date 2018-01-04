06b7fdd1395c17f72a4132a97d7f89fd5ced24a3
stevez <1024>
2018-01-04 22:13:13 +0800

    update

--- src/main/java/com/springmvc/modelattribute/controller/ModelAttributeController.java
+++ src/main/java/com/springmvc/modelattribute/controller/ModelAttributeController.java
@@ -15,11 +15,25 @@ public class ModelAttributeController {
 
     private static final String SUCCESS = "success";
 
+    /**
+     *
+     * @param id
+     * @param map
+     *
+     * pojo入参过程
+     * 1、若无@ModelAttribute及@SessionAttributes标识，则通过反射创建pojo
+     * 2、若有@ModelAttribute注解的方法，则会通过该方法获取pojo, 并放入map, 默认的key为类名第一个字母小写， 也可用@ModelAttribute标识目标方法的入参从而定义key
+     * 3、若无@ModelAttribute注解的方法、则会检查是否有@SessionAttributes注解，若有，将在session中通过key获取value
+     * 4、若有以上标识，却获取不到value则抛出异常
+     */
+
     @ModelAttribute
     public void getUser(@RequestParam(value = "id", required = false) Integer id, Map<String, Object> map){
         if( id != null ){
             User user = new User(1,"jim",12);
+            User user1 = new User(1,"jim",21);
             map.put("user",user);
+            map.put("user01",user1);
         }
 
     }
@@ -29,5 +43,12 @@ public class ModelAttributeController {
         System.out.println("****** user" + user);
         return SUCCESS;
     }
+
+    @RequestMapping("/modelattribute2")
+    public String modelAttributeTest02(@ModelAttribute("user01") User user){
+        System.out.println("****** user" + user);
+        return SUCCESS;
+    }
+
 
 }
